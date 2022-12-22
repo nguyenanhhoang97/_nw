cdb88dd02675cb47cc1e31b2641b3d16b2697b39
hoangna <hoangna@HCMHOH02HHUL671s-MacBook-Pro.local>
2022-12-22 09:21:56 +0700

    feat: 🎸 done!

---
+++ zsh-nw-custom-cmds.sh
@@ -0,0 +1,24 @@
+# custom network commands
+
+# change to use LAN
+function _nwl() {
+  networksetup -switchtolocation l
+  clear
+  echo 'Changed to use LAN'
+}
+
+# change to use WIFI
+function _nww() {
+  networksetup -switchtolocation w
+  clear
+  echo 'Changed to use WIFI'
+}
+
+# help
+function _nw-help() {
+  echo '_nw by nguyenanhhoang97\n
+  1. _nwl: change to use LAN\n
+  2. _nwl: change to use WIFI\n
+  3. _nw-help: help\n
+  '
+}
